e02ea7967d70ed5852e3db03cc0429ff30c887f1
SoftwareRacer <79541316+SoftwareRacer@users.noreply.github.com>
2021-03-13 11:03:06 +0100

    Update Tic_Tac_Toe_Console_App.cs

--- Tic_Tac_Toe_Console_App.cs
+++ Tic_Tac_Toe_Console_App.cs
@@ -4,6 +4,7 @@ namespace Tic_Tac_Toe_Console_App
 {
     class Tic_Tac_Toe_Console_App
     {
+        // Funktion zur Suche, ob 3 in einer Reihe vorhanden
         private static int threeInARow(char[,] game_field)
         {
             int state = 0, crosses = 0;
@@ -69,6 +70,7 @@ namespace Tic_Tac_Toe_Console_App
             return state;
         }
 
+        // Fragt ab, ob Feld bereits gewählt wurde
         private static bool fieldAvailable(int field_row, int field_col, char[,] game_field)
         {
             if (game_field[field_row, field_col] == 'X' || game_field[field_row, field_col] == 'O')
@@ -79,6 +81,7 @@ namespace Tic_Tac_Toe_Console_App
             return true;
         }
 
+        // Gibt das Feld im aktuellen Zustand neu aus
         private static void drawField(char[,] game_field)
         {
             Console.WriteLine("_______");
@@ -94,84 +97,93 @@ namespace Tic_Tac_Toe_Console_App
             Console.WriteLine("_______");
 
         }
+
         static void Main(string[] args)
         {
-            char[,] game_field =
+            string input = "j";
+
+            do
             {
+                char[,] game_field =
+                {
                 {'0','1','2'},
                 {'3','4','5'},
                 {'6','7','8'}
             };
-            int field_num = 9;
+                int field_num = 9;
 
-            Console.WriteLine("_______");
+                Console.WriteLine("_______");
 
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
+                for (int i = 0; i < 3; i++)
                 {
-                    Console.Write("|" + game_field[i, j]);
+                    for (int j = 0; j < 3; j++)
+                    {
+                        Console.Write("|" + game_field[i, j]);
+                    }
+                    Console.WriteLine("|");
                 }
-                Console.WriteLine("|");
-            }
-            Console.WriteLine("_______");
+                Console.WriteLine("_______");
 
-            bool isXTurn = true;
-            do
-            {
-                if (isXTurn)
+                bool isXTurn = true;
+                do
                 {
-                    do
+                    if (isXTurn)
                     {
                         do
                         {
-                            Console.WriteLine("Spieler 1(X), geben Sie die gewünschte Feldnummer ein: ");
+                            do
+                            {
+                                Console.WriteLine("Spieler 1(X), geben Sie die gewünschte Feldnummer ein: ");
+                                try
+                                {
+                                    field_num = Convert.ToInt32(Console.ReadLine());
+                                    Console.WriteLine(field_num);
+                                }
+                                catch (FormatException)
+                                {
+                                    Console.WriteLine("Input kann nicht zu Zahlenwert konvertiert werden!");
+                                }
+                            } while (!(field_num >= 0 && field_num <= 8));
+                        } while (!fieldAvailable((field_num / 3), (field_num % 3), game_field));
+                        game_field[(field_num / 3), (field_num % 3)] = 'X';
+                        isXTurn = false;
+                    }
+                    else
+                    {
+                        do
+                        {
+                            Console.WriteLine("Spieler 2(O), geben Sie die gewünschte Feldnummer ein: ");
                             try
                             {
                                 field_num = Convert.ToInt32(Console.ReadLine());
-                                Console.WriteLine(field_num);
                             }
-                            catch (FormatException)
+                            catch (Exception)
                             {
                                 Console.WriteLine("Input kann nicht zu Zahlenwert konvertiert werden!");
                             }
-                        } while(!(field_num >= 0 && field_num <= 8));
-                    } while (!fieldAvailable((field_num / 3), (field_num % 3), game_field));
-                    game_field[(field_num / 3), (field_num % 3)] = 'X';
-                    isXTurn = false;
-                }
-                else
-                {
-                    do
-                    {
-                        Console.WriteLine("Spieler 2(O), geben Sie die gewünschte Feldnummer ein: ");
-                        try
-                        {
-                            field_num = Convert.ToInt32(Console.ReadLine());
-                        }
-                        catch (Exception)
-                        {
-                            Console.WriteLine("Input kann nicht zu Zahlenwert konvertiert werden!");
-                        }
-                    } while (!fieldAvailable(field_num / 3, field_num % 3, game_field));
-                    game_field[field_num / 3, field_num % 3] = 'O';
-                    isXTurn = true;
-                }
-                drawField(game_field);
-            } while (threeInARow(game_field) == 0);
+                        } while (!fieldAvailable(field_num / 3, field_num % 3, game_field));
+                        game_field[field_num / 3, field_num % 3] = 'O';
+                        isXTurn = true;
+                    }
+                    drawField(game_field);
+                } while (threeInARow(game_field) == 0);
 
-            switch (threeInARow(game_field))
-            {
-                case 1:
-                    Console.WriteLine("Spielende: Spieler 1(X) hat drei in einer Reihe!");
-                    break;
-                case 2:
-                    Console.WriteLine("Spielende: Spieler 2(O) hat drei in einer Reihe!");
-                    break;
-                case -1:
-                    Console.WriteLine("Spielende: Unentschieden. Alle Felder belegt!");
-                    break;
-            }
+                switch (threeInARow(game_field))
+                {
+                    case 1:
+                        Console.WriteLine("Spielende: Spieler 1(X) hat drei in einer Reihe!");
+                        break;
+                    case 2:
+                        Console.WriteLine("Spielende: Spieler 2(O) hat drei in einer Reihe!");
+                        break;
+                    case -1:
+                        Console.WriteLine("Spielende: Unentschieden. Alle Felder belegt!");
+                        break;
+                }
+                Console.WriteLine("Wollen Sie ein neues Spiel beginnen? (J/N)");
+                input = Console.ReadLine();
+            } while (input == "J" || input == "j");
+            Console.WriteLine("Spiel wurde beendet. Noch einen schönen Tag!");
         }
     }
 }
